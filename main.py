724c2ec47ec132301a7aae3fcf3fa75eef01be7d
cwz <chenweize1109@163.com>
2024-01-02 16:51:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x = int(input(":"))
+if x == 2024:
+    print("HAPPY NEW YEAR!!!!!")
+if x == 1109:
+    print("HAPPY NEW YEAR!!!!!")
+if x== 1128:
+    print("HAPPY BIRTHDAY!!!!!")
+if x == 2024 or x== 1109 or x == 1128:
+    print("You are great, you have done your life very well! Trust yourself, and believe everything will be better.")
